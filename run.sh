b9b1b8b4d4f19f1d680c662aca22133d1db15c17
Mamun Miah <mmiah0@gmail.com>
2020-08-20 11:38:13 -0700

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# Use this shell script to compile (if necessary) your code and then execute it. Belw is an example of what might be found in this file if your program was written in Python 3.7
+#python ./src/consumer_complaints.py ./input/complaints.csv ./output/report.csv
+
+python ./src/consumer_complaints.py ./insight_testsuite/tests/test_1/input/complaints.csv ./insight_testsuite/tests/test_1/output/test1_report.csv
